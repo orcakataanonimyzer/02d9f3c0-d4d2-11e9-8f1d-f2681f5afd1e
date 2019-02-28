cd542aeccaa2409e686339403291ab52c5e05d74
someone <something@apa.com>
2019-02-28 07:05:54 -0500

    Added a pencil write test

--- test/test_pencil.py
+++ test/test_pencil.py
@@ -51,6 +51,12 @@ class TestPencil(unittest.TestCase):
         self.assertEqual(p.pointDurability, 39998)
         self.assertEqual(written_text, "A")
 
+    def test_write_one_uppercase_letter_decrease_point_durabilty_by_two(self):
+        p = Pencil(pencilLength=4, eraserDurability=4)
+        written_text = p.pencil_write(text_to_write="AB")
+        self.assertEqual(p.pointDurability, 39996)
+        self.assertEqual(written_text, "AB")
+
 
 if __name__ == '__main__':
     unittest.main()

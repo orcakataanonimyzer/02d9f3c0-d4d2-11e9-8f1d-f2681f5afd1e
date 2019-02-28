ef8128da6a97745dfbf57cf96a50939dde91d02f
someone <something@apa.com>
2019-02-28 05:41:57 -0500

    Added test to erase

--- test/test_pencil.py
+++ test/test_pencil.py
@@ -20,5 +20,12 @@ class TestPencil(unittest.TestCase):
         self.assertEqual(p.eraserDurability, 2)
         self.assertEqual(erased_text, "  ")
 
+
+    def test_erase_five_letters_with_eraser_durability_being_less_than_five_update_the_eraser_durability_to_have_a_string_with_one_letter_and_three_white_spaces(self):
+        p = Pencil(pencilLength=4, eraserDurability=4)
+        erased_text = p.pencil_erase(text_to_erase='abcde')
+        self.assertEqual(p.eraserDurability, 0)
+        self.assertEqual(erased_text, "a    ")
+
 if __name__ == '__main__':
     unittest.main()

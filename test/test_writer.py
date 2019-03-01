3afb9fa39aa3795ef620f8df0b67c409fe1aa4a5
someone <something@apa.com>
2019-03-01 07:31:40 -0500

    Added capability to writer to sharpen the pencil

--- test/test_writer.py
+++ test/test_writer.py
@@ -13,5 +13,14 @@ class TestWriter(unittest.TestCase):
         w.write_on_paper(' down by the sea shore')
         self.assertEqual(w.paper, 'She sells sea shells down by the sea shore')
 
+    def test_sharpen_pencil_decrease_length_by_one(self):
+        w = Write()
+        w.write_on_paper('abcdef')
+        self.assertEqual(w.pencil.pointDurability, 39994)
+        w.sharpen_pencil()
+        self.assertEqual(w.pencil.pointDurability, 40000)
+        self.assertEqual(w.pencil.pencilLength, 3)
+
+
 if __name__ == '__main__':
     unittest.main()
--- write.py
+++ write.py
@@ -8,3 +8,6 @@ class Write():
     def write_on_paper(self, desired_text_to_write):
         text_to_write = self.pencil.pencil_write(desired_text_to_write)
         self.paper += text_to_write
+
+    def sharpen_pencil(self):
+        self.pencil.pencil_sharpen()

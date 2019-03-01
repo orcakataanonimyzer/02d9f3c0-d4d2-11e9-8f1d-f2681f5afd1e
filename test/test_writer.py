581a611df061368aff389e8484f427a8d744b666
someone <something@apa.com>
2019-03-01 07:26:01 -0500

    Added test to test for multiple string writting

--- test/test_writer.py
+++ test/test_writer.py
@@ -7,5 +7,11 @@ class TestWriter(unittest.TestCase):
         w.write_on_paper('She sells sea shells')
         self.assertEqual(w.paper, 'She sells sea shells')
 
+    def test_write_two_strings_on_paper_both_strings_should_be_concatinated(self):
+        w = Write()
+        w.write_on_paper('She sells sea shells')
+        w.write_on_paper(' down by the sea shore')
+        self.assertEqual(w.paper, 'She sells sea shells down by the sea shore')
+
 if __name__ == '__main__':
     unittest.main()
